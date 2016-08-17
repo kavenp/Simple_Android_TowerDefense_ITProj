26ea6f16cbbf6bda9cfb2ce3a6488f8b3210e3dc
kavenp <kavenrulz1@gmail.com>
2016-08-17 17:29:22 +1000

    Tower creation (Mouseclick) and Game Controller Spawn Mobs

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject enemy;
+    public Vector3 spawnValues;
+
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    void Start ()
+    {
+        StartCoroutine (SpawnWaves ());
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds (startWait);
+
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3 (Random.Range (0, spawnValues.x), spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate (enemy, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds (spawnWait);
+        }
+
+        yield return new WaitForSeconds (waveWait);
+    }
+    
+}
---
+++ Assets/Scripts/Touch_Tile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch_Tile : MonoBehaviour
+{
+    public GameObject tower;
+
+
+    //    void Start ()
+    //    {
+    //    }
+    //
+    //    void Update ()
+    //    {
+    //        onMouseDown ();
+    //    }
+
+    void OnMouseDown ()
+    {
+		Vector3 position = new Vector3 (this.transform.position.x,
+			                   tower.transform.position.y, this.transform.position.z);
+		Instantiate (tower, position, tower.transform.rotation); 
+    }
+        
+}
