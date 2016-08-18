4d75bbd60073799107e1ea9a1b9b617a2932e996
traillj <traillj@users.noreply.github.com>
2016-08-18 20:40:05 +1000

    Pressing on a tower shows it's range, releasing hides it. Prevented multiple towers in a single spot.

--- Assets/Scripts/TouchTower.cs
+++ Assets/Scripts/TouchTower.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TouchTower : MonoBehaviour {
+public class TouchTower : MonoBehaviour
+{
 
+    private GameObject rangeCanvas;
 
+    void Start()
+    {
+        rangeCanvas = gameObject.transform.Find("Range Canvas").gameObject;
+        rangeCanvas.SetActive(false);
+    }
 
     void OnMouseDown()
     {
-
+        rangeCanvas.SetActive(true);
     }
+
+    void OnMouseUp()
+    {
+        rangeCanvas.SetActive(false);
+    }
+
 }
--- Assets/Scripts/Touch_Tile.cs
+++ Assets/Scripts/Touch_Tile.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class Touch_Tile : MonoBehaviour
 {
     public GameObject tower;
-    public GameObject rangeImage;
 
-    //    void Start ()
-    //    {
-    //    }
-    //
-    //    void Update ()
-    //    {
-    //        onMouseDown ();
-    //    }
+    private Object createdTower = null;
 
-    //Boys remember to get your goddamn function names right or shit will not work
+    //Boys remember to get your goddamn function
+    //names right or shit will not work
     void OnMouseDown ()
     {
-		Vector3 position = new Vector3 (this.transform.position.x,
-			                   tower.transform.position.y, this.transform.position.z);
-		Instantiate (tower, position, tower.transform.rotation);
-        Instantiate(rangeImage, position, rangeImage.transform.rotation);
+        if (createdTower != null)
+        {
+            return;
+        }
+
+        Vector3 towerPosition = new Vector3 (
+                               this.transform.position.x,
+			                   tower.transform.position.y,
+                               this.transform.position.z);
+
+        createdTower = Instantiate(tower, towerPosition,
+            tower.transform.rotation);
     }
-        
+
 }
