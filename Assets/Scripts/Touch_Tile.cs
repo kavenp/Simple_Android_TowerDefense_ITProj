d9b39665ce755726ca728c2f5d595e175512ffcc
traillj <traillj@users.noreply.github.com>
2016-08-18 17:56:52 +1000

    Created Tower Range prefab.

--- Assets/Scripts/Touch_Tile.cs
+++ Assets/Scripts/Touch_Tile.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Touch_Tile : MonoBehaviour
 {
     public GameObject tower;
-
+    public GameObject rangeImage;
 
     //    void Start ()
     //    {
@@ -15,12 +15,13 @@ public class Touch_Tile : MonoBehaviour
     //        onMouseDown ();
     //    }
 
-	//Boys remember to get your goddamn function names right or shit will not work
+    //Boys remember to get your goddamn function names right or shit will not work
     void OnMouseDown ()
     {
 		Vector3 position = new Vector3 (this.transform.position.x,
 			                   tower.transform.position.y, this.transform.position.z);
-		Instantiate (tower, position, tower.transform.rotation); 
+		Instantiate (tower, position, tower.transform.rotation);
+        Instantiate(rangeImage, position, rangeImage.transform.rotation);
     }
         
 }
