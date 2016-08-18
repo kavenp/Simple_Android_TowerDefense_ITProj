b0d719e71e1788f3c0d933a4d00194778e6bdc05
pyrons <LawrenceKuoch@Lawrences-MacBook-Air.local>
2016-08-18 22:51:34 +1000

    nothing

--- Assets/Scripts/Touch_Tile.cs
+++ Assets/Scripts/Touch_Tile.cs
@@ -5,6 +5,7 @@ public class Touch_Tile : MonoBehaviour
 {
     public GameObject tower;
 
+
     private Object createdTower = null;
 
     //Boys remember to get your goddamn function
@@ -17,11 +18,11 @@ public class Touch_Tile : MonoBehaviour
         }
 
         Vector3 towerPosition = new Vector3 (
-                               this.transform.position.x,
-			                   tower.transform.position.y,
-                               this.transform.position.z);
+                                    this.transform.position.x,
+                                    tower.transform.position.y,
+                                    this.transform.position.z);
 
-        createdTower = Instantiate(tower, towerPosition,
+        createdTower = Instantiate (tower, towerPosition,
             tower.transform.rotation);
     }
 
