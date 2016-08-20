c2829562017a5a1b505be8384121e4602745898f
traillj <traillj@users.noreply.github.com>
2016-08-20 22:02:21 +1000

    Message button and display created. No networking yet. To get a keyboard when the button is pressed, build as Android and run in an emulator or device.

---
+++ Assets/Scripts/MessageButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageButton : MonoBehaviour
+{
+
+    private TouchScreenKeyboard keyboard;
+    public Text msgDisplay;
+
+    void OnMouseDown()
+    {
+        Debug.Log("Use emulator to test chat.");
+
+        keyboard = TouchScreenKeyboard.Open("",
+            TouchScreenKeyboardType.Default);
+    }
+
+    void Update()
+    {
+        if (keyboard != null && keyboard.done)
+        {
+            msgDisplay.text = keyboard.text;
+        }
+    }
+
+}
