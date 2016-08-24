7d9b52b6b9c4d54df5e00ecf8da56a09f19dbad2
MATT-NOTEBOOK\Matthew <eldridgem@student.unimelb.edu.au>
2016-08-25 09:09:42 +1000

    Added pathing to enemies

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPathing : MonoBehaviour {
+
+	public GameObject currNode;
+	public float enemySpeed;
+	private Vector3 moveLoc;
+
+	// Use this for initialization
+	void Start () {
+		currNode = GameObject.Find("Node1");
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		moveLoc = currNode.transform.position;
+		transform.position = Vector3.MoveTowards(transform.position, moveLoc, Time.deltaTime * enemySpeed);
+		float dist;
+		dist = Vector3.Distance (transform.position, moveLoc);
+		if (dist < 1) {
+			currNode = currNode.GetComponent<NodeScript> ().nextNode;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeScript : MonoBehaviour {
+
+	public GameObject nextNode;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
