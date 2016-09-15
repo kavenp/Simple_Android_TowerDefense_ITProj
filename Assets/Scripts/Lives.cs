155cab6252b89ce1a73dd9a91bebe9ba44c38bcc
Jason Traill <traillj@users.noreply.github.com>
2016-09-15 14:24:29 +1000

    Added lives.

---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour
+{
+    
+    public int numLives;
+    public Text scoreDisplay;
+
+    void Start()
+    {
+        int size = (int)(Screen.dpi / 8.0f);
+        if (size > 0)
+        {
+            // Screen.dpi returns 0 if dpi cannot be determined
+            scoreDisplay.fontSize = size;
+         }
+        scoreDisplay.text = "Lives: " + numLives;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            numLives--;
+            scoreDisplay.text = "Lives: " + numLives;
+        }
+    }
+}
