7c59c14c43b9c405f9a2e9bee2d77ef51c23447a
Jason Traill <traillj@users.noreply.github.com>
2016-09-17 14:39:34 +1000

    Test for connection to server.

---
+++ Assets/Scripts/Client/TestConnectToCustomServer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+using UnityEngine.UI;
+
+// Derived from
+// http://pcrelated.net/index.php/csharp-send-and-receive-data-using-udpclient/
+public class TestConnectToCustomServer : MonoBehaviour
+{
+    public Text display;
+    private string response;
+    static UdpClient udp;
+    static IPEndPoint udpEndPoint;
+
+    private int socketPort = 9876;
+    private string nectarIP = "115.146.95.127";
+
+    void Start()
+    {
+        //GameObject text = GameObject.Find("Text");
+        //display = text.GetComponent<Text>();
+
+        string welcome = "Hello2";
+        byte[] data = Encoding.ASCII.GetBytes(welcome);
+
+        udpEndPoint = new IPEndPoint(IPAddress.Any, socketPort);
+        udp = new UdpClient(udpEndPoint);
+
+        // Send the data
+        udp.Send(data, data.Length, nectarIP, socketPort);
+
+        // Begin waiting for a response asynchronously
+        udp.BeginReceive(new AsyncCallback(UDP_IncomingData), udpEndPoint);
+    }
+
+    void Update()
+    {
+        display.text = response;
+    }
+
+    void UDP_IncomingData(IAsyncResult asyncResult)
+    {
+        byte[] data = udp.EndReceive(asyncResult, ref udpEndPoint);
+
+        response = Encoding.UTF8.GetString(data);
+        Debug.Log(response);
+
+        udp.Close();
+    }
+}
