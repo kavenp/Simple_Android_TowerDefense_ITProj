1342d43f0e0d68f64519b561959fd7dcfa359355
kavenp <kavenrulz1@gmail.com>
2016-09-20 15:47:26 +1000

    Simple chat UI without networking implemented

---
+++ Assets/Scripts/ChatBoxFunctions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatBoxFunctions : MonoBehaviour {
+
+	[SerializeField] Transform chatWindow;
+	[SerializeField] GameObject newMessagePrefab;
+
+	public string message = "";
+
+	public void SetMessage (string message) {
+		this.message = message;
+	}
+
+	public void ShowMessage () {
+		if (message != "") {
+			GameObject newMsg = (GameObject)Instantiate (newMessagePrefab);
+			newMsg.transform.SetParent (chatWindow);
+			newMsg.transform.SetSiblingIndex (chatWindow.childCount);
+			newMsg.GetComponent<MessageFunctions> ().ShowMessage (message);
+		}
+	}
+}
