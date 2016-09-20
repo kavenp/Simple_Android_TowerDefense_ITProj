4fb14077580754ef79c79b17fae865bed313748d
kaflu <goldenchrysanthem@gmail.com>
2016-09-20 12:47:35 +1000

    Auto go to matchmaking

--- Assets/Scripts/Multiplayer/MP_GameCoordinator.cs
+++ Assets/Scripts/Multiplayer/MP_GameCoordinator.cs
@@ -3,63 +3,61 @@ using UnityEngine.Networking;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class MP_GameCoordinator : NetworkBehaviour {
-
+public class MP_GameCoordinator : NetworkBehaviour
+{
     public GameObject enemy;
-	public Vector3 spawnValues;
+    public Vector3 spawnValues;
 
-	public int numberOfWaves;
-	public int numberOfEnemiesPerWave;
-	public float spawnWait;
-	public float waveWait;
+    public int numberOfWaves;
+    public int numberOfEnemiesPerWave;
+    public float spawnWait;
+    public float waveWait;
 
-	bool isSpawning = false;
-	bool isWave = false;
+    bool isSpawning = false;
+    bool isWave = false;
 
     void Update()
     {
-
-		if(isSpawning == false && numberOfEnemiesPerWave > 0)
-		{
-			isSpawning = true;
-			StartCoroutine(SpawnEnemyWave(spawnWait));
-		}
+        if (isSpawning == false && numberOfEnemiesPerWave > 0)
+        {
+            isSpawning = true;
+            StartCoroutine(SpawnEnemyWave(spawnWait));
+        }
 
         if (isWave == false && numberOfWaves > 0)
-		{
-			isWave = true;
-			StartCoroutine(SpawnNextWave(waveWait));
-		}
+        {
+            isWave = true;
+            StartCoroutine(SpawnNextWave(waveWait));
+        }
         else if (numberOfWaves == 0 && isWave == false &&
             numberOfEnemiesPerWave == 0 &&
             GameObject.FindWithTag("Enemy") == null)
-		{
+        {
             SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
         }
     }
 
     IEnumerator SpawnEnemyWave(float seconds)
-	{
-		yield return new WaitForSeconds(seconds);
+    {
+        yield return new WaitForSeconds(seconds);
 
-		Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		var createdEnemy = (GameObject)Instantiate(enemy, spawnPosition, spawnRotation);
-		NetworkServer.Spawn (createdEnemy);
+        Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        var createdEnemy = (GameObject)Instantiate(enemy, spawnPosition, spawnRotation);
+        NetworkServer.Spawn(createdEnemy);
 
-		isSpawning = false;
-		numberOfEnemiesPerWave -= 1;
-	}
+        isSpawning = false;
+        numberOfEnemiesPerWave -= 1;
+    }
 
-	IEnumerator SpawnNextWave(float seconds)
-	{
-		yield return new WaitForSeconds(seconds);
+    IEnumerator SpawnNextWave(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
 
-		isWave = false;
-		numberOfWaves -= 1;
+        isWave = false;
+        numberOfWaves -= 1;
 
-		// This is terrible but can fix it later on
-		numberOfEnemiesPerWave = 5;
-	}
-
+        // This is terrible but can fix it later on
+        numberOfEnemiesPerWave = 5;
+    }
 }
---
+++ Assets/Scripts/Multiplayer/MP_HostGame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MP_HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private uint roomSize = 6;
+    private string roomName;
+    private NetworkManager nm;
+
+    void Start()
+    {
+        // Get instance of network manager
+        nm = NetworkManager.singleton;
+
+        // Start matchmaking automatically
+        if (nm.matchMaker == null)
+        {
+            nm.StartMatchMaker();
+        }
+    }
+
+
+    public void SetRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoome()
+    {
+        if (roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players");
+
+            // Create room
+            nm.matchMaker.CreateMatch(roomName, roomSize, true, "", null, null, 0, 1, nm.OnMatchCreate);
+        }
+    }
+
+}
