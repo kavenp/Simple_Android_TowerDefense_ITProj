5f9d0c2d3707b02ea180d7d1bbc0c3ab2b5090c1
Matthew <eldridgem@student.unimelb.edu.au>
2016-09-23 20:23:56 +1000

    First stage of replay finished
    Automatically records the replay of a match in progress (as soon as a room is created, it starts replaying), the replay can then be viewed by running the Replay scene
    
    Currently only replays the tower spawns and player movement (but it records all necessary parts)
    
    Also, the replay map doesn't appear to be viewable for me, and I'm not 100% sure why (I just copied and pasted it from the MP scene, so it should work)

---
+++ Assets/Scripts/Replay/playReplay.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+public class playReplay : MonoBehaviour {
+
+    // Filename for the temporary file to hold the replay
+	const string fileName = "tempReplay.dat";
+	//const string fileName = "Example Replays\MPReplay.dat";
+	
+	public GameObject tower; // For spawning towers
+	public GameObject player; // For spawning players
+	
+	float startTime; // Start time of playback (used for synchronization)
+
+	GameObject[] currentPlayers;
+	
+	GameObject[] players;
+	byte[] replay; // The replay file
+	int replayIndex; // The current point in the replay file
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	    players = new GameObject[10];
+	    replay = System.IO.File.ReadAllBytes(fileName); // Read in the replay file
+	    replayIndex = 0;
+		
+		// Skip the header
+	    while(replay[replayIndex]!=2){
+		    replayIndex+=1;
+	    }
+	    replayIndex+=1;
+		
+		// Initialize the start time
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	    // If end of replay reached
+	    while(replayIndex < replay.Length){
+	
+	        // Begin update OpCode (used as error checking)
+	        if(replay[replayIndex]==11){
+	            replayIndex+=1; // Move to next OpCode
+			
+			    float timeStamp = (float)BitConverter.ToDouble(replay,replayIndex);
+			
+			
+			    // If updates have become de-synched, skip this update
+			    if((Time.time-startTime) < timeStamp){
+				    replayIndex-=1;
+				    Debug.Log("Backstep!");
+				    return;
+			    }else{
+			        replayIndex+=8;
+			    }
+		    }
+		    currentPlayers = GameObject.FindGameObjectsWithTag("Player");
+		
+		    // While receiving update player OpCodes
+		    while(replay[replayIndex]==3){
+			    replayIndex+=1; // Move to first parameter
+			
+			    replayIndex+=4; // Skip the ID number, possibly implemented later
+			
+			    //Read the xPos, zPos and yRotation of player
+			    float xPos = (float)BitConverter.ToDouble(replay,replayIndex);
+			    replayIndex+=8;
+			
+			    float zPos = (float)BitConverter.ToDouble(replay,replayIndex);
+		        replayIndex+=8;
+			
+			    float yRot = (float)BitConverter.ToDouble(replay,replayIndex);
+			    replayIndex+=8;
+			
+			    Vector3 playerPosition = new Vector3(xPos,10,zPos);
+			    Quaternion playerRotation = Quaternion.Euler(0,yRot,0);
+			
+			    // If no more players to move around (this is a new player),
+			    // spawn them, otherwise move a different player to this spot
+			    if(currentPlayers.Length == 0){
+				    GameObject play = (GameObject)Instantiate(player,playerPosition,playerRotation);
+				    play.GetComponent<MP_PlayerController>().enabled = false;
+			    }else{
+			        GameObject movePlayer = currentPlayers[currentPlayers.Length-1];
+				    movePlayer.transform.position = playerPosition;
+				    movePlayer.transform.rotation = playerRotation;
+				
+				    // Remove any moved players so they don't get moved again
+				    currentPlayers = removeObject(currentPlayers.Length-1,currentPlayers);
+			    }
+			
+		    }
+		
+		    // Skip despawn towers for now, will be implemented later
+		    while(replay[replayIndex]==5){
+		        replayIndex+=17;
+		    }
+		
+		    GameObject[] towers;
+		    towers = GameObject.FindGameObjectsWithTag("Tower");
+		
+		    // While receiving Spawn Tower OpCodes
+		    while(replay[replayIndex]==7){
+		        replayIndex+=1; // Move to first parameter
+			
+			    // Read in xPos and zPos of tower
+		        float xPos = (float)BitConverter.ToDouble(replay,replayIndex);
+		        replayIndex+=8;
+		        float zPos = (float)BitConverter.ToDouble(replay,replayIndex);
+		        replayIndex+=8;
+		    
+		    	// Spawn the tower
+		        Vector3 towerPosition = new Vector3(xPos,10,zPos);
+		        Instantiate(tower,towerPosition,tower.transform.rotation);
+		    }
+		
+		    // Skip to the end of Update, will implement other things later
+		    while(replay[replayIndex]!=12){
+		        replayIndex+=1;
+		    }
+		
+		    replayIndex+=1;
+	        }
+		}
+	
+	// Removes an object from an array
+	GameObject[] removeObject(int loc,GameObject[] list){
+	
+	    // Move the object to the end of the array
+		for(int i = loc; i < list.Length-1; i++){
+		    list[loc] = list[loc+1];
+		}
+		
+		// Delete the end of the array (containing the object to delete)
+		if(loc < list.Length){
+			Array.Resize(ref list,list.Length - 1);
+		}
+		
+		return list;	
+	}
+}
---
+++ Assets/Scripts/Replay/recordReplay.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+public class recordReplay : MonoBehaviour {
+	
+	// Filename for the temporary file to hold the replay
+	const string fileName = "tempReplay.dat";
+	
+	// List of the towers currently spawned
+	List<GameObject> currentTowers = new List<GameObject>();
+	
+	// The starting time of the recording (used for synchronization)
+	float startTime;
+	
+	// Use this for initialization
+	void Start () {
+	
+	    startTime = Time.time;
+	
+	    // Writing the header for the replay file, should be the size of
+		// the float type, and size of the int type on the current system
+		// Currently not used, but may be required later
+	    var stream = new FileStream(fileName, FileMode.Create);
+	    
+	    stream.Write(new byte[]{1}, 0, 1); // Begin header OpCode
+	    
+	    stream.Write(new byte[]{sizeof(float)}, 0, 1);
+	    
+	    stream.Write(new byte[]{sizeof(int)}, 0, 1);
+	    
+	    stream.Write(new byte[]{2},0,1); // End header OpCode
+	    
+	    stream.Close();
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		
+		// Recording doesn't start until game has started
+		if(players.Length < 1)
+		{
+			startTime = Time.time;
+		    return;
+		}
+		
+		var stream = new FileStream(fileName, FileMode.Append);
+		
+		stream.Write(new byte[]{11},0,1); //Update Start OpCode
+		
+		byte [] byteStream;
+		
+		// Record the timestamp for the current update
+		byteStream = BitConverter.GetBytes((double)(Time.time-startTime));
+		stream.Write(byteStream,0,byteStream.Length);
+		
+		// Records which player is recorded
+		int index=0;
+		
+		// For each player
+		foreach(GameObject i in players){
+		    index+=1;
+		    stream.Write(new byte[]{3},0,1); // Player update OpCode
+		    
+			// Record ID of the player
+		    byteStream = BitConverter.GetBytes(index);
+		    stream.Write(byteStream,0,byteStream.Length);
+		    
+			// Record the players position
+		    recordPosition(byteStream,stream,i);
+		    
+			// Record the players rotation
+		    byteStream = BitConverter.GetBytes((double)i.transform.eulerAngles.y);
+		    stream.Write(byteStream,0,byteStream.Length);
+		}
+		
+		GameObject[] towers = GameObject.FindGameObjectsWithTag("Tower");
+		
+		// For each tower that has been recorded as existing
+		foreach(GameObject i in currentTowers){
+		    
+			// If the tower doesn't actually exist, record a despawn event
+		    int isIn = Array.IndexOf(towers,i);
+		    if(isIn <= -1){
+		        stream.Write(new byte[]{5},0,1); // Tower despawn OpCode
+		    
+			    recordPosition(byteStream,stream,i);
+			
+			    currentTowers.Remove(i);
+			
+		    }
+		}
+		
+		// For each tower that actually exists
+		foreach(GameObject i in towers){
+		    
+			// If the tower has not been recorded as existing, record the spawn event
+		    if(!currentTowers.Contains(i)){
+		        stream.Write(new byte[]{7},0,1); // Tower spawn OpCode
+			
+			    recordPosition(byteStream,stream,i);
+			
+			    currentTowers.Add(i);
+		    }
+		}
+		
+		int gold = players[0].GetComponent<MP_PlayerController>().GetGold();
+		
+		stream.Write(new byte[]{9},0,1); // Set gold value OpCode
+		
+		byteStream = BitConverter.GetBytes(gold);
+	    stream.Write(byteStream,0,byteStream.Length);
+		
+		GameObject playerBase = GameObject.Find("PlayerBase");
+		int lives = playerBase.GetComponent<LivesScript>().numLives;
+		
+		stream.Write(new byte[]{10},0,1); // Set lives value OpCode
+		
+		byteStream = BitConverter.GetBytes(lives);
+	        stream.Write(byteStream,0,byteStream.Length);
+		
+		stream.Write(new byte[]{12},0,1); // End Update OpCode
+		
+		stream.Close();
+		
+	
+	}
+	
+	// Records an (x,z) position of an object to the bytefile
+	private void recordPosition(byte [] byteStream, FileStream stream, GameObject i){
+		byteStream = BitConverter.GetBytes((double)i.transform.position.x);
+		stream.Write(byteStream,0,byteStream.Length);
+		    
+		byteStream = BitConverter.GetBytes((double)i.transform.position.z);
+		stream.Write(byteStream,0,byteStream.Length);
+	}
+}
