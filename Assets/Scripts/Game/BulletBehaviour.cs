a6008f4210f549247f863ac8c6fe1413918ffa99
pyrons <LawrenceKuoch@Lawrences-MacBook-Air.local>
2016-09-27 11:07:58 +1000

    syncing of enemy death:

--- Assets/Scripts/Game/BulletBehaviour.cs
+++ Assets/Scripts/Game/BulletBehaviour.cs
@@ -4,75 +4,76 @@ using System.Collections;
 
 public class BulletBehaviour : NetworkBehaviour
 {
-	private int damage = 10;
-	//bullet damage;
-	public float speed = 5;
-	//bullets speed
-	public GameObject target = null;
-	//bullets target (Enemy)
-	public Vector3 startpos;
-	//bullets start position
-	public Vector3 targetpos;
-	//bullets target position
+    private int damage = 3;
+    //bullet damage;
+    public float speed = 5;
+    //bullets speed
+    [SyncVar]
+    public GameObject target = null;
+    //bullets target (Enemy)
+    public Vector3 startpos;
+    //bullets start position
+    public Vector3 targetpos;
+    //bullets target position
 
-	//public GameController gc;
-	//game controller, interfaces do not appear in Unity inspector
-	private EnemyHealth enemyHP;
-	//enemy HP
-	private float distance;
-	//distance from target
-	private float startTime;
+    //public GameController gc;
+    //game controller, interfaces do not appear in Unity inspector
+    private EnemyHealth enemyHP;
+    //enemy HP
+    private float distance;
+    //distance from target
+    private float startTime;
     //when bullet first shot
 
     private Vector3 previousPosition;
     //private float existTime = 0;
     //private int maxTime = 5;
 
-	// Use this for initialization
-	void Start ()
-	{
-		startTime = Time.time;
-		distance = Vector3.Distance (startpos, targetpos);
-		//EnemyDestructionDelegate dele = target.gameObject.GetComponent<EnemyDestructionDelegate>();
-		//dele.enemyDelegate += OnEnemyDestroy;
-		enemyHP = (EnemyHealth) target.GetComponent ("EnemyHealth");
-		//GameObject gcObject = GameObject.FindGameObjectWithTag ("GameController");
+    // Use this for initialization
+    void Start ()
+    {
+        startTime = Time.time;
+        distance = Vector3.Distance (startpos, targetpos);
+        //EnemyDestructionDelegate dele = target.gameObject.GetComponent<EnemyDestructionDelegate>();
+        //dele.enemyDelegate += OnEnemyDestroy;
+        enemyHP = (EnemyHealth) target.GetComponent ("EnemyHealth");
+        //GameObject gcObject = GameObject.FindGameObjectWithTag ("GameController");
 
-		//gc = gcObject.GetComponent<GameController> ();
+        //gc = gcObject.GetComponent<GameController> ();
 
-	}
+    }
 
-	// Self destructs once enemy is detected to be destroyed
-	void OnEnemyDestroy (GameObject target)
-	{
-		Destroy (gameObject);
-	}
+    // Self destructs once enemy is detected to be destroyed
+    void OnEnemyDestroy (GameObject target)
+    {
+        Destroy (gameObject);
+    }
 
-	// Update is called once per frame
-	void Update ()
-	{
-		float timeInt = Time.time - startTime;
-		//time interval from start to current frame
+    // Update is called once per frame
+    void Update ()
+    {
+        float timeInt = Time.time - startTime;
+        //time interval from start to current frame
 
-		gameObject.transform.position = Vector3.Lerp (startpos, targetpos, timeInt * speed / distance);
-		//linearly interpolating from starting position to target considering speed
-		startpos = gameObject.transform.position;
+        gameObject.transform.position = Vector3.Lerp (startpos, targetpos, timeInt * speed / distance);
+        //linearly interpolating from starting position to target considering speed
+        startpos = gameObject.transform.position;
 
-        if (startpos.Equals(previousPosition))
+        if (startpos.Equals (previousPosition))
         {
-            Destroy(gameObject);
+            Destroy (gameObject);
         }
 
         previousPosition = startpos;
         if (target != null)
-		{
-			targetpos = target.transform.position;
-		}
-		else
-		{
-			Destroy (gameObject);
-			//target is null so destroy bullet;
-		}
+        {
+            targetpos = target.transform.position;
+        }
+        else
+        {
+            Destroy (gameObject);
+            //target is null so destroy bullet;
+        }
 
 
 //		if (gameObject.transform.position.Equals (targetpos)) {
@@ -90,29 +91,33 @@ public class BulletBehaviour : NetworkBehaviour
 //		}
     }
 
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.gameObject == target)
-		{
-			enemyHP.Hit (damage);
-			if (enemyHP.health <= 0)
-			{
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == target)
+        {
+            enemyHP.Hit (damage);
+            if (enemyHP.health <= 0)
+            {
                 Destroy (other.gameObject);
-                Destroy(gameObject);
+                Destroy (gameObject);
             }
         }
-	}
+    }
+
+    public void SetTarget (GameObject _target)
+    {
+        this.target = _target;
+    }
 
 
 
-
-	public void AddDamageToBullet(int damage)
-	{
+    public void AddDamageToBullet (int damage)
+    {
         this.damage += damage;
     }
 
-	public void AddSpeedToBullet(int speed)
-	{
+    public void AddSpeedToBullet (int speed)
+    {
         this.speed += speed;
     }
 
--- Assets/Scripts/Game/ShootEnemies.cs
+++ Assets/Scripts/Game/ShootEnemies.cs
@@ -5,135 +5,184 @@ using System.Collections.Generic;
 
 public class ShootEnemies : NetworkBehaviour
 {
-	public List<GameObject> inRange;
-	//list of enemies in range
-	public GameObject projectilePrefab;
-	//for instantiating projectile
-	private float lastShotTime;
-	//time of last shot
-	//private GameController gc;
-	static private double fireRate = 1;
+    public List<GameObject> inRange;
+    //list of enemies in range
+    public GameObject projectilePrefab;
+    //for instantiating projectile
+    private float lastShotTime;
+    //time of last shot
+    //private GameController gc;
+    static private double fireRate = 1;
 
-	//[SyncVar(hook = "AddAdditionalDamage")]
-	private int additionalDamage = 0;
+    //[SyncVar(hook = "AddAdditionalDamage")]
+    private int additionalDamage = 0;
     private Vector3 previousTargetLoc;
 
     // Number of upgrades
     [SyncVar]
     public int level;
 
-	// Use this for initialization
-	void Start ()
-	{
-		inRange = new List<GameObject> ();
-		lastShotTime = Time.time;
+    private NetworkIdentity objNetId;
 
-		// Get the type of tower and modify its starting damage values upon creation
-		if(this.tag == "Tower")
-		{
+    // Use this for initialization
+    void Start ()
+    {
+        inRange = new List<GameObject> ();
+        lastShotTime = Time.time;
+
+        // Get the type of tower and modify its starting damage values upon creation
+        if (this.tag == "Tower")
+        {
             this.additionalDamage = 0;
         }
 
-		if (this.tag == "Tower2")
-		{
+        if (this.tag == "Tower2")
+        {
             this.additionalDamage = 12;
         }
 
-		if (this.tag == "Tower3")
-		{
+        if (this.tag == "Tower3")
+        {
             this.additionalDamage = 23;
         }
 
         level = 0;
     }
 
-	void OnEnemyDestroy (GameObject enemy)
-	{
-		inRange.Remove (enemy);
-		//once destroyed remove enemy from list
-	}
+    void OnEnemyDestroy (GameObject enemy)
+    {
+        inRange.Remove (enemy);
+        //once destroyed remove enemy from list
+    }
 
-	//Shoot the enemy
-	//[Command]
-	void Shoot (GameObject target)
-	{
-		Vector3 startloc = gameObject.transform.position;
-		Vector3 targetloc = target.transform.position;
+    //Shoot the enemy
+    //[Command]
+    void Shoot (GameObject target)
+    {
+        Vector3 startloc = gameObject.transform.position;
+        Vector3 targetloc = target.transform.position;
 
-        if (targetloc.Equals(previousTargetLoc))
+        if (targetloc.Equals (previousTargetLoc))
         {
-            inRange.Remove(target);
+            inRange.Remove (target);
             return;
         }
 
-		//get starting and target positions
-		GameObject newBullet = (GameObject) Instantiate (projectilePrefab, startloc, projectilePrefab.transform.rotation);
+        CmdSpawnBullet (startloc, targetloc, target);
 
-		//instantiate the new bullet
-		BulletBehaviour behavior = (BulletBehaviour) newBullet.GetComponent ("BulletBehaviour");
-        //behavior.gc = gc;
-        behavior.AddDamageToBullet(this.additionalDamage);
-        behavior.target = target;
-		behavior.startpos = startloc;
-		behavior.targetpos = targetloc;
-        previousTargetLoc = targetloc;
-        //NetworkServer.Spawn(newBullet);
+        if (objNetId != null)
+        {
+            objNetId.RemoveClientAuthority (connectionToClient);    
+        }
+
+
+        //get starting and target positions
     }
 
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.gameObject.tag.Equals ("Enemy"))
-		{
-			inRange.Add (other.gameObject);
-			EnemyDestructionDelegate dele = other.gameObject.GetComponent<EnemyDestructionDelegate> ();
-			dele.enemyDelegate += OnEnemyDestroy;
-		}
-	}
+    [ClientRpc]
+    void RpcSpawnBullet (GameObject newBullet, Vector3 startloc, Vector3 targetloc, GameObject target)
+    {
+        newBullet = (GameObject) Instantiate (projectilePrefab, startloc, projectilePrefab.transform.rotation);
 
-	void OnTriggerExit (Collider other)
-	{
-		if (other.gameObject.tag.Equals ("Enemy"))
-		{
-			inRange.Remove (other.gameObject);
-			EnemyDestructionDelegate dele = other.gameObject.GetComponent<EnemyDestructionDelegate> ();
-			dele.enemyDelegate -= OnEnemyDestroy;
-		}
-	}
+        BulletBehaviour behavior = (BulletBehaviour) newBullet.GetComponent ("BulletBehaviour");
+        //behavior.gc = gc;
+        behavior.AddDamageToBullet (this.additionalDamage);
+        behavior.target = target;
+        behavior.startpos = startloc;
+        behavior.targetpos = targetloc;
 
-	void Update ()
-	{
-		GameObject target = null;
-		//initialize null target
-		//float minDist = float.MaxValue;
-		if (inRange.Count != 0)
-		{
-			target = inRange [0];
-		}
-		//choose first enemy in list to be target if list is non-empty
-		if (target != null)
-		{
-			//start shooting if target exists
-			if (Time.time - lastShotTime > fireRate)
-			{
-				//shoot if the time between now and last shot is larger than set fire rate
-				Shoot (target);
-				lastShotTime = Time.time;
-				//update time of last shot
-			}
-		}
-	}
+        previousTargetLoc = targetloc;
+    }
 
-	public void AddAdditionalDamage(int damage)
-	{
+
+    [Command]
+    void CmdSpawnBullet (Vector3 startloc, Vector3 targetloc, GameObject target)
+    {
+        GameObject newBullet = (GameObject) Instantiate (projectilePrefab, startloc, projectilePrefab.transform.rotation);
+
+        objNetId = gameObject.GetComponent<NetworkIdentity> ();
+        if (objNetId != null)
+        {
+            objNetId.RemoveClientAuthority (connectionToClient);    
+            objNetId.AssignClientAuthority (connectionToClient);    
+        }
+
+
+
+        //instantiate the new bullet
+        BulletBehaviour behavior = (BulletBehaviour) newBullet.GetComponent ("BulletBehaviour");
+        //behavior.gc = gc;
+        behavior.AddDamageToBullet (this.additionalDamage);
+        //behavior.target = target;
+        behavior.SetTarget (target);
+        behavior.startpos = startloc;
+        behavior.targetpos = targetloc;
+
+        previousTargetLoc = targetloc;
+
+        NetworkServer.Spawn (newBullet);
+
+
+
+        RpcSpawnBullet (newBullet, startloc, targetloc, target);
+    }
+
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag.Equals ("Enemy"))
+        {
+            inRange.Add (other.gameObject);
+            EnemyDestructionDelegate dele = other.gameObject.GetComponent<EnemyDestructionDelegate> ();
+            dele.enemyDelegate += OnEnemyDestroy;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (other.gameObject.tag.Equals ("Enemy"))
+        {
+            inRange.Remove (other.gameObject);
+            EnemyDestructionDelegate dele = other.gameObject.GetComponent<EnemyDestructionDelegate> ();
+            dele.enemyDelegate -= OnEnemyDestroy;
+        }
+    }
+
+    void Update ()
+    {
+        GameObject target = null;
+        //initialize null target
+        //float minDist = float.MaxValue;
+        if (inRange.Count != 0)
+        {
+            target = inRange[0];
+        }
+        //choose first enemy in list to be target if list is non-empty
+        if (target != null)
+        {
+            //start shooting if target exists
+            if (Time.time - lastShotTime > fireRate)
+            {
+                //shoot if the time between now and last shot is larger than set fire rate
+                Shoot (target);
+                lastShotTime = Time.time;
+                //update time of last shot
+            }
+        }
+    }
+
+    public void AddAdditionalDamage (int damage)
+    {
         this.additionalDamage += damage;
     }
 
 
-	//	void OnTriggerStay (Collider other) {
-	//		if (other.gameObject.tag.Equals ("Enemy")) {
-	//			inRange.Add (other.gameObject);
-	//			EnemyDestructionDelegate dele = other.gameObject.GetComponent<EnemyDestructionDelegate>();
-	//			dele.enemyDelegate += OnEnemyDestroy;
-	//		}
-	//	}
+    //	void OnTriggerStay (Collider other) {
+    //		if (other.gameObject.tag.Equals ("Enemy")) {
+    //			inRange.Add (other.gameObject);
+    //			EnemyDestructionDelegate dele = other.gameObject.GetComponent<EnemyDestructionDelegate>();
+    //			dele.enemyDelegate += OnEnemyDestroy;
+    //		}
+    //	}
 }
