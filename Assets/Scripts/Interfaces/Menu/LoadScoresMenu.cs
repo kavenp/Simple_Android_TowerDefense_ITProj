1f7636e7d9307fe02bcd467c6391af74da9d855d
Jason Traill <traillj@users.noreply.github.com>
2016-10-08 20:24:40 +1100

    Fixes to background colours. Added links to the scores menu.

---
+++ Assets/Scripts/Interfaces/Menu/LoadScoresMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScoresMenu : MonoBehaviour
+{
+    // Load the scores menu when clicked
+    public void OnClick()
+    {
+        SceneManager.LoadScene("ScoresMenu", LoadSceneMode.Single);
+    }
+}
